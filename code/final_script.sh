0f29fd16d6a58a13214733f3809e26c0196ff24e
taekseuk <taekseuk@andrew.cmu.edu>
2025-04-28 19:09:37 -0400

    script

---
+++ code/final_script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Define the list of files
+FILES=(
+  "RV11/BB11015.tfa"
+  "RV11/BB11028.tfa"
+  "RV11/BBS11015.tfa"
+  "RV12/BB12004.tfa"
+  "RV12/BBS12011.tfa"
+)
+
+LONGFEW=(
+  "RV11/BB11016.tfa"
+  "RV11/BB11026.tfa"
+  "RV11/BB11037.tfa"
+  "RV11/BB11038.tfa"
+)
+# Loop through each file
+for file in "${LONGFEW[@]}"; do
+    # Extract the filename (e.g., BB20001.tfa)
+    base_filename=$(basename "$file" .tfa)
+
+    # Paths
+    input_path="../data/bb3_release/${file}"
+    output_path="../data/${base_filename}.out"
+
+    # Run sequential version and save output
+    echo "Running bm_seq on ${file}"
+    ./bm_seq -i "${input_path}" -o temp_seq_output.txt > temp_seq_terminal.txt 2>&1
+
+    # Run parallel version and save output
+    echo "Running bm_par on ${file}"
+    mpirun -np 8 ./bm_par -i "${input_path}" -o temp_par_output.txt > temp_par_terminal.txt 2>&1
+
+    # Combine the terminal outputs into a single .out file
+    {
+      echo "===== Sequential Run Output ====="
+      cat temp_seq_terminal.txt
+      echo ""
+      echo "===== Parallel Run Output ====="
+      cat temp_par_terminal.txt
+    } > "${output_path}"
+
+    # Clean up temporary files
+    rm temp_seq_output.txt temp_par_output.txt temp_seq_terminal.txt temp_par_terminal.txt
+
+    # Info
+    echo "Processed $file -> ${output_path}"
+done
--- code/seq_run.sh
+++ code/seq_run.sh
@@ -1,13 +1,39 @@
 echo "Hi"
 
 # List of files you want to process
+# FILES=(
+#  # "RV12/BBS12014.tfa
+#  # "RV40/BB40007.tfa"
+#  # "RV30/BB30029.tfa"
+#   "RV20/BBS20036.tfa"
+#  # "RV30/BB30003.tfa"
+# )
+
+# FILES2=(
+#   "RV11/BB11001.tfa"
+#   "RV11/BBS11002.tfa"
+#   "RV12/BB12002.tfa"
+#   "RV12/BB12004.tfa"
+#   "RV12/BB12008.tfa"
+# )
+
+# Around similar as BF12008, where I expect around 100 seconds to run
 FILES=(
- # "RV12/BBS12014.tfa
- # "RV40/BB40007.tfa"
- # "RV30/BB30029.tfa"
+  "RV12/BBS12014.tfa"
   "RV20/BBS20036.tfa"
- # "RV30/BB30003.tfa"
+  "RV20/BB20001.tfa"
+  "RV12/BB12013.tfa"
+  "RV12/BBS12011.tfa"
+  "RV20/BB20007.tfa"
 )
+
+
+# # Similar as BF12008, around twice longer time expected
+# FILES4=(
+#   "RV20/BB20004.tfa"
+#   "RV20/BB20006.tfa"
+#   "RV20/BBS20006.tfa"
+# )
 
 # Loop through each file
 for file in "${FILES[@]}"; do
